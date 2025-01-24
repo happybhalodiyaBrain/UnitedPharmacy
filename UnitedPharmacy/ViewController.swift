a3be9de3320ac7412d055029adf8fe5930c8fc49
happy.bhalodiya <happy.bhalodiya@brainvire.com>
2025-01-24 18:52:07 +0530

    Initial Commit

---
+++ UnitedPharmacy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitedPharmacy
+//
+//  Created by Happy  Bhalodiya on 24/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
